58438d5f6fad66706fd74cd24369b075f0803aed
Kino Mondesir <kino.mondesir@gmail.com>
2019-07-20 20:43:45 -0400

    update task script

--- task.ps1
+++ task.ps1
@@ -6,8 +6,9 @@ try {
   Import-VstsLocStrings "$PSScriptRoot\task.json"
 
   # Get inputs.
-  $input_source = Get-VstsInput -Name 'source' -Require
-  $input_target = Get-VstsInput -Name 'destination' -Require
+  
+  [string]$input_source = Get-VstsInput -Name 'source' -Require
+  [string]$input_target = Get-VstsInput -Name 'destination' -Require
 
   # Execute utility script containing logic
   Invoke-Expression -Command $PSScriptRoot\utility.ps1 -Source $input_source -Destination $input_target
